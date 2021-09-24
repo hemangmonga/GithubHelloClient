cf36f9b1637a40a396faa270bc741b87f43c35f3
afrozchakure <aaaanchakure@gmail.com>
2021-09-23 21:37:19 -0700

    Afroz's changes

---
+++ src/main/java/com/p1/HelloObject.java
@@ -0,0 +1,13 @@
+package com.p1;
+
+public class HelloObject {
+	 private String message;
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+}
