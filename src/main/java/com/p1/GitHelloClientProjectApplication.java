8cfd449591844e85efcd5ebf447ec87f24ba6392
afrozchakure <aaaanchakure@gmail.com>
2021-09-23 22:11:47 -0700

    Final changes

--- src/main/java/com/p1/GitHelloClientProjectApplication.java
+++ src/main/java/com/p1/GitHelloClientProjectApplication.java
@@ -2,9 +2,9 @@ package com.p1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@EnableEurekaServer
+@EnableDiscoveryClient
 @SpringBootApplication
 public class GitHelloClientProjectApplication {
 
